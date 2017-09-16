b42014bf1e8a92110a346a4e95501b43464a233c
dewshare <32006488+dewshare@users.noreply.github.com>
2017-09-16 08:51:53 +0500

    Add files via upload

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+require_once("database.php"); 
+  
+				$sql = "select * from email_lists  ";
+				$res = mysql_query($sql);
+				if($res)
+				{
+					if(mysql_num_rows($res) > 0)
+					{
+						while($row = mysql_fetch_assoc($res))
+						{		
+						
+							if(emailAlreadyExists($email,"log","email") == false )
+							{
+								$sql = "insert into log (email) values ('".$email."')";
+								$r = mysql_query($sql);
+							}
+							
+					}
+				 
+				}
+			
+?>
